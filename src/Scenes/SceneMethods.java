c1b9f5c1547e68c88ca105d1eb0aaec8ec3a6294
Liam Bottcher <lbottcher171@gmail.com>
2025-11-09 21:47:44 -0500

    [implement] video 7

--- src/Scenes/SceneMethods.java
+++
@@ -1,8 +0,0 @@
-package Scenes;
-
-import java.awt.Graphics;
-
-public interface SceneMethods {
-	public void render(Graphics g);
-	public void mouseClicked(int x, int y);
-}
--- src/main/GameStates.java
+++
@@ -1,8 +0,0 @@
-package main;
-
-public enum GameStates {
-	
-	PLAYING, MENU, SETTINGS;
-	
-	public static GameStates gameState = MENU;
-}
